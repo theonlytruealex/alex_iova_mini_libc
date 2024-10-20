834b522c481356c7b3ad24a7788f6a6ca749f260
theonlytruealex <alexiova264@gmail.com>
2024-10-20 21:50:30 +0300

    Started strings

--- src/string/string.c
+++ src/string/string.c
@@ -4,38 +4,95 @@
 
 char *strcpy(char *destination, const char *source)
 {
-	/* TODO: Implement strcpy(). */
+	int i = 0;
+	while (source[i] != '\0') {
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = '\0';
 	return destination;
 }
 
 char *strncpy(char *destination, const char *source, size_t len)
 {
-	/* TODO: Implement strncpy(). */
+	int i;
+	for (i = 0; i < len; i++) {
+		if (source[i] == '\0') {
+			break;
+		}
+		destination[i] = source[i];
+	}
+	for (i; i < len; i++) {
+		destination[i] = '\0';
+	}
 	return destination;
 }
 
 char *strcat(char *destination, const char *source)
 {
-	/* TODO: Implement strcat(). */
+	int i = 0, len = strlen(destination);
+	while (source[i] != '\0') {
+		destination[len + i] = source[i];
+		i++;
+	}
+	destination[i + len] = '\0';
 	return destination;
 }
 
 char *strncat(char *destination, const char *source, size_t len)
 {
-	/* TODO: Implement strncat(). */
+	int i = 0, len2 = strlen(destination);
+	while (source[i] != '\0' && i < len) {
+		destination[len2 + i] = source[i];
+		i++;
+	}
+	destination[i + len2] = '\0';
 	return destination;
 }
 
 int strcmp(const char *str1, const char *str2)
 {
-	/* TODO: Implement strcmp(). */
-	return -1;
+	int i = 0;
+	while (str1[i] != '\0' && str2[i] !='\0') {
+		if (str1[i] > str2[i]) {
+			return 1;
+		}
+		if (str1[i] < str2[i]) {
+			return -1;
+		}
+		i++;
+	}
+	if (str1[i] > str2[i]) {
+		return 1;
+	}
+	if (str1[i] < str2[i]) {
+		return -1;
+	}
+	return 0;
 }
 
 int strncmp(const char *str1, const char *str2, size_t len)
 {
-	/* TODO: Implement strncmp(). */
-	return -1;
+	int i = 0;
+	while (str1[i] != '\0' && str2[i] !='\0' && i < len) {
+		if (str1[i] > str2[i]) {
+			return 1;
+		}
+		if (str1[i] < str2[i]) {
+			return -1;
+		}
+		i++;
+	}
+	if (i == len) {
+		return 0;
+	}
+	if (str1[i] > str2[i]) {
+		return 1;
+	}
+	if (str1[i] < str2[i]) {
+		return -1;
+	}
+	return 0;
 }
 
 size_t strlen(const char *str)
@@ -50,37 +107,91 @@ size_t strlen(const char *str)
 
 char *strchr(const char *str, int c)
 {
-	/* TODO: Implement strchr(). */
+	int i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == c) {
+			return str + i;
+		}
+		i++;
+	}
 	return NULL;
 }
 
 char *strrchr(const char *str, int c)
 {
-	/* TODO: Implement strrchr(). */
+	int i = strlen(str) - 1;
+	while (i >= 0) {
+		if (str[i] == c) {
+			return str + i;
+		}
+		i--;
+	}
 	return NULL;
 }
 
 char *strstr(const char *haystack, const char *needle)
 {
-	/* TODO: Implement strstr(). */
+	int i = 0;
+	while (haystack[i] != '\0') {
+		int j = 0;
+		while (haystack[i + j] != '\0' && needle[j] != '\0' && haystack[i + j] == needle[j])
+			j++;
+		if (needle[j] == '\0') {
+			return haystack + i;
+		}
+		i++;
+	}
 	return NULL;
 }
 
 char *strrstr(const char *haystack, const char *needle)
 {
-	/* TODO: Implement strrstr(). */
+	int i = strlen(haystack), needle_len = strlen(needle);
+	while (i >= 0) {
+		int j = 0;
+		while (i + j >= 0 && needle_len + j > 1 && haystack[i + j] == needle[needle_len - 1 + j])
+			j--;
+		if (needle_len + j == 1) {
+			return haystack + i - needle_len + 1;
+		}
+		i--;
+	}
 	return NULL;
 }
 
 void *memcpy(void *destination, const void *source, size_t num)
 {
-	/* TODO: Implement memcpy(). */
+	int i;
+	char* string_dst = (char*)destination;
+	char* string_src = (char*)source;
+
+	// The function does not check for any terminating null character in source -
+	//it always copies exactly num bytes.
+	for (i = 0; i < num; i++) {
+		string_dst[i] = string_src[i];
+	}
 	return destination;
 }
 
 void *memmove(void *destination, const void *source, size_t num)
 {
-	/* TODO: Implement memmove(). */
+	int i;
+	char* string_dst = (char*)destination;
+	char* string_src = (char*)source;
+	char buffer[num];
+
+	// The function does not check for any terminating null character in source -
+	//it always copies exactly num bytes.
+	if (destination < source) {
+		for (i = 0; i < num; i++) {
+			buffer[i] = string_src[i];
+		}
+	}
+	else {
+		for (i = num - 1; i >= 0; i--) {
+			string_dst[i] = string_src[i];
+		}
+	}
 	return destination;
 }
 
