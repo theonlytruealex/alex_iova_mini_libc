e765479f4f7386c3b6d2f7c50f541e91793f4c5c
theonlytruealex <alexiova264@gmail.com>
2024-10-23 11:15:13 +0300

    Added: check for flags and fds in mmap

--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -6,7 +6,19 @@
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	void * res = syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
+
+	if ((flags & MAP_ANONYMOUS) == 0) {
+		int ok_files = syscall(__NR_fcntl, fd, 1);
+		if (ok_files < 0) {
+			errno = EBADF;
+			return MAP_FAILED;
+		}
+	}
+	if (((flags & MAP_PRIVATE) ^ (flags & MAP_SHARED)) == 0) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+	void *res = syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
 	return res;
 }
 
